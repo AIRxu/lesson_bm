24ab2bae21a07e17cd40cb74ef882139af133ceb
AIRxu <43132415+AIRxu@users.noreply.github.com>
2020-04-01 23:26:28 +0800

    拖动改变视频播放速度demo

---
+++ h5/video/index.js
@@ -0,0 +1,44 @@
+// // 获取dom结构的js  documen.querySelector('.speed') 类名用. id用#
+// const speed = document.querySelector('.speed');
+// const bar = document.querySelector('.speed-bar');
+// const video = document.querySelector('.flex');
+
+// console.log(video.offsetTop)
+
+// function handlemove(e) {
+//   // console.log(e.pageY);
+//   const y = e.pageY - this.offsetTop;
+//   const percent = y / this.offsetHeight;
+//   const min = 0.4;
+//   const max = 4;
+//   const height = Math.round(percent * 100)
+// }
+
+// // 当鼠标在speed上发生mousemove事件的时候，handlemove函数就会执行
+// speed.addEventListener('mousemove',handlemove);
+
+
+
+const speed = document.querySelector('.speed')
+const bar = document.querySelector('.speed-bar')
+const video = document.querySelector('.flex')
+
+// console.log(video.offsetHeight)
+
+function handleMove(e) {
+  // console.log(e.pageY)
+  const y = e.pageY - this.offsetTop; // offsetTop获取某个dom结构到浏览器顶部的距离
+  const percent = y / this.offsetHeight; // offsetHeight用来获取某dom结构的自身高度
+  const min = 0.4;
+  const max = 4;
+  const height = Math.round(percent * 100) + '%';
+  const playbackRate = percent * ( max - min ) + min;
+
+  // console.log(y, percent, height, playbackRate)
+  bar.style.height = height
+  bar.textContent = playbackRate.toFixed(2) + 'x'
+  video.playbackRate = playbackRate
+}
+
+
+speed.addEventListener('mousemove', handleMove)
